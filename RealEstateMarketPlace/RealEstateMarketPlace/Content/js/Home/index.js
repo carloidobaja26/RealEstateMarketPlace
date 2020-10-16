5dbae3463d5fd200f0fdd5726aec04cc4a75be9f
carloidobaja26 <onewress@gmail.com>
2020-10-16 21:23:35 +0800

    SP1

--- RealEstateMarketPlace/RealEstateMarketPlace/Content/js/Home/index.js
+++ RealEstateMarketPlace/RealEstateMarketPlace/Content/js/Home/index.js
@@ -4,13 +4,18 @@
         $('#modalRent').modal('show')
     });
     $('#ulid').on('click', function (e) {
-        if ($("#ulid").css('display').toLowerCase() != 'block') {
-            $("#ulid").css("display", "block");
-        }
-        else {
-            $("#ulid").css("display", "hidden");
-        }
-        //e.stopPropagation();
+        //if ($("#ulid").css('display').toLowerCase() != 'block') {
+        //    $("#ulid").css("display", "block");
+        //}
+        //else {
+        //    $("#ulid").css("display", "hidden");
+        //}
+        e.stopPropagation();
+    });
+    $("#ex25").slider({
+        value: [100, 10000],
+        ticks: [100, 2000,4000,6000,8000, 10000],
+        lock_to_ticks: false
     });
 });
 
